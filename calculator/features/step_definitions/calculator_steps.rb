5ae32eceb7234d55626eb6055917225d8c718f5b
Brent Johnson <bjohnson@monsooncommerce.com>
2015-08-20 14:06:41 -0700

    ch 2.7 with calc.rb this time

--- calculator/features/step_definitions/calculator_steps.rb
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -8,6 +8,6 @@ When(/^the calculator is run\?\?$/) do
 end
 
 
-Then /^the output should be "([^"]*)"$/ do |arg1|
-pending # express the regexp above with the code you wish you had??
+Then /^the output should be "([^"]*)"$/ do |expected_output|
+  @output.should == expected_output
 end
